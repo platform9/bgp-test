e328deffdee01f0b6993bbb2dd3863ccd11c8fb0
Shubham Mali <shubham@platform9.com>
2024-05-09 21:17:21 +0530

    rename ns

--- pod-comm-test/pod-pod-comm.sh
+++ pod-comm-test/pod-pod-comm.sh
@@ -1,5 +1,5 @@
 # Create Namespace
-kubectl create namespace test
+kubectl create namespace pod-comm-test
 
 # Create Test Deployment
 kubectl apply -f - <<EOF
@@ -8,7 +8,7 @@ apiVersion: apps/v1
 kind: Deployment
 metadata:
   name: test-deployment
-  namespace: test
+  namespace: pod-comm-test
 spec:
   replicas: 10
   selector:
@@ -33,7 +33,7 @@ apiVersion: v1
 kind: Service
 metadata:
   name: test-service
-  namespace: test
+  namespace: pod-comm-test
 spec:
   selector:
     app: test
@@ -45,32 +45,32 @@ EOF
 
 test_pod_to_pod_comms() {
   
-  deployment_pods=$(kubectl get pods -n test -l app=test -o jsonpath='{.items[*].metadata.name}')
+  deployment_pods=$(kubectl get pods -n pod-comm-test -l app=test -o jsonpath='{.items[*].metadata.name}')
   deployment_pod_array=($deployment_pods)
 
   # Pick two random pods from different nodes
   pod1=${deployment_pod_array[$((RANDOM % ${#deployment_pod_array[@]}))]}
-  node1=$(kubectl get pod -n test $pod1 -o jsonpath='{.spec.nodeName}')
+  node1=$(kubectl get pod -n pod-comm-test $pod1 -o jsonpath='{.spec.nodeName}')
 
   pod2=""
   node2=""
   while [ -z "$pod2" ] || [ "$node1" == "$node2" ]; do
     pod2=${deployment_pod_array[$((RANDOM % ${#deployment_pod_array[@]}))]}
-    node2=$(kubectl get pod -n test $pod2 -o jsonpath='{.spec.nodeName}')
+    node2=$(kubectl get pod -n pod-comm-test $pod2 -o jsonpath='{.spec.nodeName}')
   done
 
-  pod1_ip=$(kubectl get pod -n test $pod1 -o jsonpath='{.status.podIP}')
-  pod2_ip=$(kubectl get pod -n test $pod2 -o jsonpath='{.status.podIP}')
+  pod1_ip=$(kubectl get pod -n pod-comm-test $pod1 -o jsonpath='{.status.podIP}')
+  pod2_ip=$(kubectl get pod -n pod-comm-test $pod2 -o jsonpath='{.status.podIP}')
 
   # Test pod-to-pod communication
-  kubectl exec -n test $pod1 -- curl -s http://$pod2_ip:80 >/dev/null 2>&1
+  kubectl exec -n pod-comm-test $pod1 -- curl -s http://$pod2_ip:80 >/dev/null 2>&1
   if [ $? -eq 0 ]; then
     echo "Pod-to-pod communication succeeded: NODE:$node1  POD:$pod1_ip  ->  NODE:$node2  POD:$pod2_ip"
   else
     echo "Pod-to-pod communication failed: NODE:$node1  POD:$pod1_ip  ->  NODE:$node2  POD:$pod2_ip"
   fi
 
-  kubectl exec -n test $pod2 -- curl -s http://$pod1_ip:80 >/dev/null 2>&1
+  kubectl exec -n pod-comm-test $pod2 -- curl -s http://$pod1_ip:80 >/dev/null 2>&1
   if [ $? -eq 0 ]; then
     echo "Pod-to-pod communication succeeded: NODE:$node2  POD:$pod2_ip  ->  NODE:$node1  POD:$pod1_ip"
   else
@@ -79,37 +79,23 @@ test_pod_to_pod_comms() {
 }
 
 test_pod_to_service_comms() {
-  service_ip=$(kubectl get svc -n test test-service -o jsonpath='{.spec.clusterIP}')
+  service_ip=$(kubectl get svc -n pod-comm-test test-service -o jsonpath='{.spec.clusterIP}')
 
   # Pick a random pod
   pod=${deployment_pod_array[$((RANDOM % ${#deployment_pod_array[@]}))]}
-  node=$(kubectl get pod -n test $pod -o jsonpath='{.spec.nodeName}')
+  node=$(kubectl get pod -n pod-comm-test $pod -o jsonpath='{.spec.nodeName}')
 
   # Get the pod IP address
-  pod_ip=$(kubectl get pod -n test $pod -o jsonpath='{.status.podIP}')
+  pod_ip=$(kubectl get pod -n pod-comm-test $pod -o jsonpath='{.status.podIP}')
 
   # Test pod-to-service communication
-  kubectl exec -n test $pod -- curl -s http://$service_ip:80 >/dev/null 2>&1
+  kubectl exec -n pod-comm-test $pod -- curl -s http://$service_ip:80 >/dev/null 2>&1
   if [ $? -eq 0 ]; then
     echo "Pod-to-service communication succeeded: NODE:$node  POD:$pod_ip  ->  SERVICE:$service_ip"
   else
     echo "Pod-to-service communication failed: NODE:$node  POD:$pod_ip  ->  SERVICE:$service_ip"
   fi
 }
-
-# # test with different replica counts
-# for replicas in 10 100 20 50 ; do
-#   kubectl scale -n test deployment/test-deployment --replicas=$replicas
-#   kubectl rollout status -n test deployment/test-deployment --timeout=60s
-
-#   for ((i = 1; i <= 10; i++)); do
-#     test_pod_to_pod_comms
-#     test_pod_to_service_comms()
-#   done
-
-#   echo "Waiting for 10 seconds..."
-#   sleep 10
-# done
 
 # Initial replica count
 min_replicas=10
@@ -118,8 +104,8 @@ replicas=50
 
 while [ $min_replicas -lt $max_replicas ]; do
   # Scale the deployment to the current replica count
-  kubectl scale -n test deployment/test-deployment --replicas=$replicas
-  kubectl rollout status -n test deployment/test-deployment --timeout=60s
+  kubectl scale -n pod-comm-test deployment/test-deployment --replicas=$replicas
+  kubectl rollout status -n pod-comm-test deployment/test-deployment --timeout=60s
 
   # Test pod-to-pod and pod-to-service communication
   for ((i = 1; i <= 10; i++)); do
@@ -144,6 +130,10 @@ while [ $min_replicas -lt $max_replicas ]; do
       ;;
     q)
       # Quit
+      read -p "delete the deployment and service? y/n: " action
+      if [ "$action" == "y" ]; then
+        kubectl delete namespace pod-comm-test
+      fi
       break
       ;;
     *)
@@ -151,5 +141,3 @@ while [ $min_replicas -lt $max_replicas ]; do
       ;;
   esac
 done
-
-kubectl delete namespace test
